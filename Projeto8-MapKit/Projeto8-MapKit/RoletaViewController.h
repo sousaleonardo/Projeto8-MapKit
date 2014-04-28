b9d8a7079017146125b6834a4186bf8b4e36c73f
Julia <julia.a.c.yamamoto@gmail.com>
2014-04-28 15:40:35 -0300

    Criada viewController que irá controlar o desafio

---
+++ Projeto8-MapKit/Projeto8-MapKit/RoletaViewController.h
@@ -0,0 +1,15 @@
+//
+//  RoletaViewController.h
+//  Projeto8-MapKit
+//
+//  Created by JULIA ARISSA COPPOLA YAMAMOTO on 28/04/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoletaViewController : UIViewController
+
+
+
+@end
---
+++ Projeto8-MapKit/Projeto8-MapKit/RoletaViewController.m
@@ -0,0 +1,41 @@
+//
+//  RoletaViewController.m
+//  Projeto8-MapKit
+//
+//  Created by JULIA ARISSA COPPOLA YAMAMOTO on 28/04/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import "RoletaViewController.h"
+
+@interface RoletaViewController ()
+
+@end
+
+@implementation RoletaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
