e0bdc958ff5aa2f02b39749acc0fec24ab7ebe65
Julia <julia.a.c.yamamoto@gmail.com>
2014-04-28 16:49:49 -0300

    Recriada classe RoletaViewController

--- Projeto8-MapKit/Projeto8-MapKit/RoletaViewController.m
+++ Projeto8-MapKit/Projeto8-MapKit/RoletaViewController.m
@@ -18,7 +18,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-    
+        // Custom initialization
     }
     return self;
 }
@@ -26,16 +26,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    
-    
+    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    
+    // Dispose of any resources that can be recreated.
 }
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
